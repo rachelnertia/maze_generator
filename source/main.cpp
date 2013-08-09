abcf81a477df2535f6ec5fa8bdeea26391ddd193
Nersh <nershly@gmail.com>
2013-08-09 15:55:23 +0100

    hmmph

--- source/main.cpp
+++ source/main.cpp
@@ -12,8 +12,9 @@
 #define BPP	32
 
 // size of maze
-#define ROWS	12
-#define COLUMNS	12
+#define GRID_WIDTH	20
+#define GRID_HEIGHT	20
+
 
 //File stream for the game log.
 std::ofstream logger( "log.txt" );
@@ -31,14 +32,14 @@ void log(int n) {
 }
 
 
-int GetNumberOfUnvisitedCells(Cell [ROWS][COLUMNS]);
-void DepthFirstSearch(Cell [ROWS][COLUMNS]);
-std::vector<GridRef> GetUnvisitedNeighbours(Cell [ROWS][COLUMNS], GridRef);
-void InitGrid(Cell [ROWS][COLUMNS]);
+int GetNumberOfUnvisitedCells(Cell [GRID_WIDTH][GRID_HEIGHT]);
+void DepthFirstSearch(Cell [GRID_WIDTH][GRID_HEIGHT], SDL_Surface*);
+std::vector<GridRef> GetUnvisitedNeighbours(Cell [GRID_WIDTH][GRID_HEIGHT], GridRef);
+void InitGrid(Cell [GRID_WIDTH][GRID_HEIGHT]);
 
-//void drawGrid(Cell [ROWS][COLUMNS]);
+//void drawGrid(Cell [GRID_WIDTH][GRID_HEIGHT]);
 
-void DrawGrid(Cell [ROWS][COLUMNS], SDL_Surface*);
+void DrawGrid(Cell [GRID_WIDTH][GRID_HEIGHT], SDL_Surface*);
 
 
 
@@ -67,21 +68,21 @@ int main(int argc, char* args[]) {
 	SDL_WM_SetCaption("amaze generator", NULL);
 
 	// maze grid
-	Cell grid [ROWS][COLUMNS];
+	Cell grid [GRID_WIDTH][GRID_HEIGHT];
 	log("setting up grid");
 	InitGrid(grid);
 
 	
 	// cut the maze
 	log("beginning depth first search maze generation");
-	DepthFirstSearch(grid);
+	DepthFirstSearch(grid, screen);
 
 	// draw the maze
 	log("drawing");
 	DrawGrid(grid, screen);
 
 	//SDL_BlitSurface(drawnMaze, NULL, screen, NULL);
-	SDL_Flip(screen);
+	//SDL_Flip(screen);
 
 	// 
 	log("beginning while loop");
@@ -107,9 +108,9 @@ int main(int argc, char* args[]) {
 }
 
 
-void InitGrid(Cell grid[ROWS][COLUMNS]) {
-	for (int x = 0; x < ROWS; x++) {
-		for (int y = 0; y < COLUMNS; y++) {
+void InitGrid(Cell grid[GRID_WIDTH][GRID_HEIGHT]) {
+	for (int x = 0; x < GRID_WIDTH; x++) {
+		for (int y = 0; y < GRID_HEIGHT; y++) {
 			grid[x][y] = Cell(x, y);
 		}
 	}
@@ -118,9 +119,9 @@ void InitGrid(Cell grid[ROWS][COLUMNS]) {
 
 // Make a maze out of a given grid of Cells using a Depth-first Search algorithm
 // implemented using backtracking
-void DepthFirstSearch(Cell grid[ROWS][COLUMNS]) {
+void DepthFirstSearch(Cell grid[GRID_WIDTH][GRID_HEIGHT], SDL_Surface* surface) {
 	// start at a random cell and call it the exit
-	GridRef exitCell(rand()%ROWS, rand()%COLUMNS);
+	GridRef exitCell(rand()%GRID_WIDTH, rand()%GRID_HEIGHT);
 
 	// make the initial cell the current cell and mark it as visited
 	GridRef currentCell = exitCell;
@@ -141,15 +142,19 @@ void DepthFirstSearch(Cell grid[ROWS][COLUMNS]) {
 			stack.push(grid[currentCell.x][currentCell.y]);
 
 			// remove the wall between the current cell and the chosen cell
+			// if the chosen cell is east of the current cell
 			if (chosenCell.x > currentCell.x) {
 				grid[currentCell.x][currentCell.y].connectedDirections[EAST] = 1;
 				grid[chosenCell.x][chosenCell.y].connectedDirections[WEST] = 1;
+			// if the chosen cell is west of the current cell
 			} else if (chosenCell.x < currentCell.x) {
 				grid[currentCell.x][currentCell.y].connectedDirections[WEST] = 1;
 				grid[chosenCell.x][chosenCell.y].connectedDirections[EAST] = 1;
+			// if the chosen cell is south of the current cell
 			} else if (chosenCell.y > currentCell.y) {
 				grid[currentCell.x][currentCell.y].connectedDirections[SOUTH] = 1;
 				grid[chosenCell.x][chosenCell.y].connectedDirections[NORTH] = 1;
+			// if the chosen cell is north of the current cell
 			} else if (chosenCell.y < currentCell.y) {
 				grid[currentCell.x][currentCell.y].connectedDirections[NORTH] = 1;
 				grid[chosenCell.x][chosenCell.y].connectedDirections[SOUTH] = 1;
@@ -168,64 +173,71 @@ void DepthFirstSearch(Cell grid[ROWS][COLUMNS]) {
 			currentCell = GridRef(topCell.gridRef.x, topCell.gridRef.y);
 		} else {
 			// pick a random cell, make it the current cell and mark it as visited
-			currentCell = GridRef(rand()%ROWS, rand()%COLUMNS);
+			currentCell = GridRef(rand()%GRID_WIDTH, rand()%GRID_HEIGHT);
 			grid[currentCell.x][currentCell.y].visited = true;
 		}
 
-		log("logging stack size:");
-		log(stack.size());
+		DrawGrid(grid, surface);
+		SDL_Delay(5);
 	}
 
 }
 
-int GetNumberOfUnvisitedCells(Cell grid[ROWS][COLUMNS]) {
+int GetNumberOfUnvisitedCells(Cell grid[GRID_WIDTH][GRID_HEIGHT]) {
 	int n = 0;
 
-	for (int x = 0; x < ROWS; x++) {
-		for (int y = 0; y < COLUMNS; y++) {
+	for (int x = 0; x < GRID_WIDTH; x++) {
+		for (int y = 0; y < GRID_HEIGHT; y++) {
 			if (grid[x][y].visited == false)
 				n++;
 		}
 	}
 
-	log("logging number of unvisited cells:");
-	log(n);
 	return n;
 }
 
-std::vector<GridRef> GetUnvisitedNeighbours(Cell grid[ROWS][COLUMNS], GridRef cell) {
+std::vector<GridRef> GetUnvisitedNeighbours(Cell grid[GRID_WIDTH][GRID_HEIGHT], GridRef cell) {
 	std::vector<GridRef> list;
-	if (cell.x > 1) {
+	
+	if (cell.x > 0) {
 		// check the cell to the west/left
 		if ( (grid[cell.x - 1][cell.y]).visited == false ) {
 			// add it to the list
 			list.push_back( GridRef(cell.x - 1, cell.y) );
 		}
 	}
-	if (cell.y > 1) {
-		// check the cell on top/to the north
-		if ( (grid[cell.x][cell.y - 1]).visited == false ) {
-			list.push_back( GridRef(cell.x, cell.y - 1) );
-		}
-	}
-	if (cell.x < ROWS) {
+	if (cell.x < GRID_WIDTH) {
 		// to the right/east
 		if ( (grid[cell.x + 1][cell.y]).visited == false ) {
 			list.push_back( GridRef(cell.x + 1, cell.y) );
 		}
 	}
-	if (cell.y < COLUMNS) {
+	if (cell.y > 0) {
+		// check the cell on top/to the north
+		if ( (grid[cell.x][cell.y - 1]).visited == false ) {
+			list.push_back( GridRef(cell.x, cell.y - 1) );
+		}
+	}
+	if (cell.y < GRID_HEIGHT) {
 		// check the cell on top/to the north
 		if ( (grid[cell.x][cell.y + 1]).visited == false ) {
 			list.push_back( GridRef(cell.x, cell.y + 1) );
 		}
 	}
 
+	/*for (int i = 0; i <list.size(); i++) {
+		if ((list[i].x < 0) || (list[i].x > GRID_WIDTH)
+			|| (list[i].y < 0) || (list[i].y > GRID_HEIGHT)) {
+			log("a cell is out of bounds");
+		} 
+	}*/
+	
+	//log(list.size());
 	return list;
 }
 
 
-void DrawGrid(Cell grid[ROWS][COLUMNS], SDL_Surface* surface) {
+void DrawGrid(Cell grid[GRID_WIDTH][GRID_HEIGHT], SDL_Surface* surface) {
 	// fill black
 	// rect struct for filling
 	SDL_Rect fillRect;
@@ -235,46 +247,48 @@ void DrawGrid(Cell grid[ROWS][COLUMNS], SDL_Surface* surface) {
 	fillRect.h = SCREEN_HEIGHT;
 	SDL_FillRect(surface, &fillRect, 0x000000);
 
-	for (int x = 0; x < ROWS; x++) {
-		for (int y = 0; y < COLUMNS; y++) {
+	for (int x = 0; x < GRID_WIDTH; x++) {
+		for (int y = 0; y < GRID_HEIGHT; y++) {
 			// fill a rectangle in the middle
 			if (grid[x][y].visited) {
-				fillRect.x = x*(SCREEN_WIDTH/COLUMNS) + ((SCREEN_WIDTH/COLUMNS)/4);
-				fillRect.y = y*(SCREEN_HEIGHT/ROWS) + ((SCREEN_HEIGHT/ROWS)/4);
-				fillRect.w = (SCREEN_WIDTH/COLUMNS)/2;
-				fillRect.h = (SCREEN_HEIGHT/ROWS)/2;
+				fillRect.x = x*(SCREEN_WIDTH/GRID_WIDTH) + ((SCREEN_WIDTH/GRID_WIDTH)/4);
+				fillRect.y = y*(SCREEN_HEIGHT/GRID_HEIGHT) + ((SCREEN_HEIGHT/GRID_HEIGHT)/4);
+				fillRect.w = (SCREEN_WIDTH/GRID_WIDTH)/2;
+				fillRect.h = (SCREEN_HEIGHT/GRID_HEIGHT)/2;
 				SDL_FillRect(surface, &fillRect, 0xFFFFFF);
 			}
 			// draw connections
 			if (grid[x][y].connectedDirections[NORTH]) {
-				fillRect.x = x*(SCREEN_WIDTH/COLUMNS) + ((SCREEN_WIDTH/COLUMNS)/4);
-				fillRect.y = y*(SCREEN_HEIGHT/ROWS);
-				fillRect.w = (SCREEN_WIDTH/COLUMNS)/2;
-				fillRect.h = (SCREEN_HEIGHT/ROWS)/4;
+				fillRect.x = x*(SCREEN_WIDTH/GRID_WIDTH) + ((SCREEN_WIDTH/GRID_WIDTH)/4);
+				fillRect.y = y*(SCREEN_HEIGHT/GRID_HEIGHT);
+				fillRect.w = (SCREEN_WIDTH/GRID_WIDTH)/2;
+				fillRect.h = (SCREEN_HEIGHT/GRID_HEIGHT)/4;
 				SDL_FillRect(surface, &fillRect, 0xFFFFFF);
 			}
 			if (grid[x][y].connectedDirections[SOUTH]) {
-				fillRect.x = x*(SCREEN_WIDTH/COLUMNS) + ((SCREEN_WIDTH/COLUMNS)/4);
-				fillRect.y = y*(SCREEN_HEIGHT/ROWS) + (((SCREEN_HEIGHT/ROWS)/4)*3);
-				fillRect.w = (SCREEN_WIDTH/COLUMNS)/2;
-				fillRect.h = (SCREEN_HEIGHT/ROWS)/4;
+				fillRect.x = x*(SCREEN_WIDTH/GRID_WIDTH) + ((SCREEN_WIDTH/GRID_WIDTH)/4);
+				fillRect.y = y*(SCREEN_HEIGHT/GRID_HEIGHT) + (((SCREEN_HEIGHT/GRID_HEIGHT)/4)*3);
+				fillRect.w = (SCREEN_WIDTH/GRID_WIDTH)/2;
+				fillRect.h = (SCREEN_HEIGHT/GRID_HEIGHT)/4;
 				SDL_FillRect(surface, &fillRect, 0xFFFFFF);
 			}
 			if (grid[x][y].connectedDirections[WEST]) {
-				fillRect.x = x*(SCREEN_WIDTH/COLUMNS);
-				fillRect.y = y*(SCREEN_HEIGHT/ROWS) + ((SCREEN_HEIGHT/ROWS)/4);
-				fillRect.w = (SCREEN_WIDTH/COLUMNS)/4;
-				fillRect.h = (SCREEN_HEIGHT/ROWS)/2;
+				fillRect.x = x*(SCREEN_WIDTH/GRID_WIDTH);
+				fillRect.y = y*(SCREEN_HEIGHT/GRID_HEIGHT) + ((SCREEN_HEIGHT/GRID_HEIGHT)/4);
+				fillRect.w = (SCREEN_WIDTH/GRID_WIDTH)/4;
+				fillRect.h = (SCREEN_HEIGHT/GRID_HEIGHT)/2;
 				SDL_FillRect(surface, &fillRect, 0xFFFFFF);
 			}			
 			if (grid[x][y].connectedDirections[EAST]) {
-				fillRect.x = x*(SCREEN_WIDTH/COLUMNS) + (((SCREEN_WIDTH/COLUMNS)/4)*3);
-				fillRect.y = y*(SCREEN_HEIGHT/ROWS) + ((SCREEN_HEIGHT/ROWS)/4);
-				fillRect.w = (SCREEN_WIDTH/COLUMNS)/4;
-				fillRect.h = (SCREEN_HEIGHT/ROWS)/2;
+				fillRect.x = x*(SCREEN_WIDTH/GRID_WIDTH) + (((SCREEN_WIDTH/GRID_WIDTH)/4)*3);
+				fillRect.y = y*(SCREEN_HEIGHT/GRID_HEIGHT) + ((SCREEN_HEIGHT/GRID_HEIGHT)/4);
+				fillRect.w = (SCREEN_WIDTH/GRID_WIDTH)/4;
+				fillRect.h = (SCREEN_HEIGHT/GRID_HEIGHT)/2;
 				SDL_FillRect(surface, &fillRect, 0xFFFFFF);
 			}
 		}
 	}
+
+	SDL_Flip(surface);
 
 }
